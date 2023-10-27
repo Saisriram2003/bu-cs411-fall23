6ab57a4576aa4a7b01afcda1c44ea5b6e0bd7ce7
Saisriram Gunturu <saisriram@saimail.com>
2023-10-27 14:17:26 -0400

    Demo for backend and frontend

---
+++ App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [news, setNews] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Define the URL of your Express backend endpoint
+    const apiUrl = 'http://localhost:5001/news';
+
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setNews(data.articles);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setError(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Top Headlines</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : (
+        <ul>
+          {news.map((article, index) => (
+            <li key={index}>
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                {article.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
+
+export default App;
